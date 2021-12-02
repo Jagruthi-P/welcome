6b38abf7a2c242fedf9e00778180fa7c27867100
Jagruthi <peyyalajagruthi2000@gmail.com>
2021-12-01 20:18:38 -0800

    first commit

---
+++ welcomejava/src/main/java/com/welcome/java/welcome.java
@@ -0,0 +1,10 @@
+package com.welcome.java;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("hello java");
+	}
+
+}
